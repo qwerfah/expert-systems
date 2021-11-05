6b50c7de701497061175c968c8a5c14ec76bdb68
qwerfah <qwerfah@gmail.com>
2021-11-05 16:28:34 +0300

    Main function updated

--- lab01/src/main/scala/com/qwerfah/search/fromdata/Main.scala
+++ lab01/src/main/scala/com/qwerfah/search/fromdata/Main.scala
@@ -1,9 +1,42 @@
 package com.qwerfah.search.fromdata
 
 import com.qwerfah.search.xml.Parser
+import com.qwerfah.search.data.{Term, State}
+import com.qwerfah.search.data.SearchData
 
 @main def hello: Unit =
-  val parser = Parser("knowledgebase.xml")
-  println(parser.parse())
+  val path = "./system.xml"
+  print(s"\nProduction system xml representation file path [$path]: ")
+  val customPath = scala.io.StdIn.readLine()
 
-def msg = "I was compiled by Scala 3. :)"
+  val parser = Parser(if customPath.isBlank then path else customPath)
+  val system = parser.parse
+  println(s"\nProduction system: $system\n")
+
+  val search = WidthSearch(system)
+  val searchData =
+    SearchData(
+      State(Set(Term("a"), Term("b"), Term("c"), Term("d"))),
+      State(Set(Term("h")))
+    )
+
+  println(search.search(searchData))
+
+/*
+  while true do
+    var initialTerms = Array[Term]()
+    var targetTerms = Array[Term]()
+
+
+    while initialTerms.isEmpty do
+      print("Space separated list of terms for initial state: ")
+      val initial = scala.io.StdIn.readLine()
+      initialTerms = initial.split(" ").filter(!_.isBlank).map(Term(_))
+      if initialTerms.isEmpty then println("Empty list, try again")
+
+    while targetTerms.isEmpty do
+      print("Space separated list of terms for final state: ")
+      val target = scala.io.StdIn.readLine()
+      targetTerms = target.split(" ").filter(!_.isBlank).map(Term(_))
+      if targetTerms.isEmpty then println("Empty list, try again")
+ */
