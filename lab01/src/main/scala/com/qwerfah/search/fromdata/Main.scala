b49eecc4a900f327224bf8b107b4a8a7d98dc83d
qwerfah <qwerfah@gmail.com>
2021-11-05 23:17:58 +0300

    Main was updated

--- lab01/src/main/scala/com/qwerfah/search/fromdata/Main.scala
+++ lab01/src/main/scala/com/qwerfah/search/fromdata/Main.scala
@@ -1,32 +1,19 @@
 package com.qwerfah.search.fromdata
 
+import scala.util.{Success, Failure}
+
 import com.qwerfah.search.xml.Parser
-import com.qwerfah.search.data.{Term, State}
+import com.qwerfah.search.dot.Serializer
 import com.qwerfah.search.data.SearchData
+import com.qwerfah.search.data.{Term, State}
+import com.qwerfah.search.data.ProductionSystem
 
-@main def hello: Unit =
-  val path = "./system.xml"
-  print(s"\nProduction system xml representation file path [$path]: ")
-  val customPath = scala.io.StdIn.readLine()
-
-  val parser = Parser(if customPath.isBlank then path else customPath)
-  val system = parser.parse
-  println(s"\nProduction system: $system\n")
-
+def loop(system: ProductionSystem) =
   val search = WidthSearch(system)
-  val searchData =
-    SearchData(
-      State(Set(Term("a"), Term("b"), Term("c"), Term("d"))),
-      State(Set(Term("h")))
-    )
 
-  println(search.search(searchData))
-
-/*
   while true do
     var initialTerms = Array[Term]()
     var targetTerms = Array[Term]()
-
 
     while initialTerms.isEmpty do
       print("Space separated list of terms for initial state: ")
@@ -39,4 +26,24 @@ import com.qwerfah.search.data.SearchData
       val target = scala.io.StdIn.readLine()
       targetTerms = target.split(" ").filter(!_.isBlank).map(Term(_))
       if targetTerms.isEmpty then println("Empty list, try again")
- */
+
+    val searchData =
+      SearchData(State(initialTerms.toSet), State(targetTerms.toSet))
+
+    search.search(searchData) match
+      case Some(graph) =>
+        println("\nTarget state is reachable\n")
+        Serializer(graph).serialize("graph.dot") match
+          case Success(_) => println("\nDecision graph saved\n")
+          case Failure(e) =>
+            println(s"\nUnable to save graph: ${e.getMessage}\n")
+      case None => println("\nTarget state is unreachable\n")
+
+@main def hello: Unit =
+  val path = "./system.xml"
+  print(s"\nProduction system xml representation file path [$path]: ")
+  val customPath = scala.io.StdIn.readLine()
+
+  Parser(if customPath.isBlank then path else customPath).parse match
+    case Success(system) => loop(system)
+    case Failure(e)      => println(s"\nUnable to parse xml: ${e.getMessage}\n")
