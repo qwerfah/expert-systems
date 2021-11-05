63db445f148e56e82e0a487f31197fda94f2f9f3
qwerfah <qwerfah@gmail.com>
2021-11-05 23:16:04 +0300

    Try monad usage was added

--- lab01/src/main/scala/com/qwerfah/search/xml/Parser.scala
+++ lab01/src/main/scala/com/qwerfah/search/xml/Parser.scala
@@ -1,31 +1,39 @@
 package com.qwerfah.search.xml
 
 import scala.xml
+import scala.util.{Try, Success, Failure}
 
 import java.io.FileReader
 import java.io._
 
+import cats.implicits._
+
 import com.qwerfah.search.data._
 
 class Parser(filename: String):
+  private def openXmlFile: Try[xml.Elem] =
+    Try {
+      xml.XML.loadFile(
+        (new File(".")).getAbsolutePath + "/resources/" + filename
+      )
+    }
 
-  val system: xml.Elem =
-    xml.XML.loadFile((new File(".")).getAbsolutePath + "/resources/" + filename)
+  private def parseAntecedents(ruleNode: xml.Node): Try[Set[Term]] =
+    Try {
+      (ruleNode \ "antecedents" \ "antecedent").map { node =>
+        Term((node \ "@name").text)
+      }.toSet
+    }
 
-  private def parseAntecedents(ruleNode: xml.Node): Set[Term] =
-    (ruleNode \ "antecedents" \ "antecedent").map { node =>
-      Term((node \ "@name").text)
-    }.toSet
+  private def parseConsequent(ruleNode: xml.Node): Try[Term] =
+    Try { Term((ruleNode \ "consequent" \ "@name").text) }
 
-  private def parseConsequent(ruleNode: xml.Node) = Term(
-    (ruleNode \ "consequent" \ "@name").text
-  )
+  private def parseRule(ruleNode: xml.Node): Try[Rule] = for
+    antecedents <- parseAntecedents(ruleNode)
+    consequent <- parseConsequent(ruleNode)
+  yield Rule(antecedents, consequent)
 
-  private def parseRule(ruleNode: xml.Node) = Rule(
-    parseAntecedents(ruleNode),
-    parseConsequent(ruleNode)
-  )
-
-  def parse = ProductionSystem(
-    (system \ "rules" \ "rule").map(parseRule _).toSet
-  )
+  def parse: Try[ProductionSystem] = for
+    system <- openXmlFile
+    rules <- (system \ "rules" \ "rule").map(parseRule _).sequence
+  yield ProductionSystem(rules.toSet)
