6ebe701d8408931e083b636af788ecf8490e246f
qwerfah <qwerfah@gmail.com>
2021-11-30 17:03:17 +0300

    Serializer marks initial and target state in graph now

--- lab01/src/main/scala/com/qwerfah/search/dot/Serializer.scala
+++ lab01/src/main/scala/com/qwerfah/search/dot/Serializer.scala
@@ -6,16 +6,21 @@ import java.io.File
 
 import com.qwerfah.search.data.StateGraph
 import java.io.FileWriter
+import com.qwerfah.search.data.SearchData
 
 class Serializer(graph: StateGraph):
   private def createWriter(filename: String): Try[FileWriter] =
     Try { new FileWriter(new File(filename)) }
 
-  private def serialize(writer: FileWriter): Try[Unit] =
+  private def serialize(writer: FileWriter, data: SearchData): Try[Unit] =
     def serialize(graph: StateGraph): Unit =
-      writer.write(s"\"[${graph.state}]\"")
+      val fillcolor =
+        if graph.state == data.initial then "\"green\""
+        else if graph.state == data.target then "\"red\""
+        else "\"white\""
+      writer.write(s"\"${graph.state}\" [style=filled fillcolor=$fillcolor]\n")
       graph.subgraphs.getOrElse(Seq[StateGraph]()) foreach { g =>
-        writer.write(s"\"[${graph.state}]\" -> \"[${g.state}]\"\n")
+        writer.write(s"\"${graph.state}\" -> \"${g.state}\"\n")
         serialize(g)
       }
 
@@ -25,9 +30,9 @@ class Serializer(graph: StateGraph):
       writer.write("}")
       writer.close()
     }
-    
-  def serialize(filename: String): Try[Unit] =
+
+  def serialize(filename: String, data: SearchData): Try[Unit] =
     for
       writer <- createWriter(filename)
-      result <- serialize(writer)
+      result <- serialize(writer, data)
     yield result
