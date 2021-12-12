d9af1c56dff2b4b65d648632bf95668cfb57c4a0
qwerfah <qwerfah@gmail.com>
2021-12-12 23:44:07 +0300

    update main

--- lab01/src/main/scala/com/qwerfah/search/fromdata/Main.scala
+++ lab01/src/main/scala/com/qwerfah/search/fromdata/Main.scala
@@ -3,48 +3,44 @@ package com.qwerfah.search.fromdata
 import scala.util.{Success, Failure}
 
 import com.qwerfah.search.xml.Parser
-import com.qwerfah.search.dot.Serializer
+import com.qwerfah.search.dot.ToDotFileSerializer
 import com.qwerfah.search.data.SearchData
 import com.qwerfah.search.data.{Term, State}
 import com.qwerfah.search.data.ProductionSystem
 
 def loop(system: ProductionSystem) =
-  val search = WidthSearch(system)
+  // while true do
+  var initialTerms = Array[Term]()
+  var targetTerms = Array[Term]()
 
-  while true do
-    var initialTerms = Array[Term]()
-    var targetTerms = Array[Term]()
+  while initialTerms.isEmpty do
+    print("Space separated list of terms for initial state: ")
+    val initial = "intention_rest place_mountains" // scala.io.StdIn.readLine()
+    initialTerms = initial.split(" ").filter(!_.isBlank).map(Term(_))
+    if initialTerms.isEmpty then println("Empty list, try again")
 
-    while initialTerms.isEmpty do
-      print("Space separated list of terms for initial state: ")
-      val initial = scala.io.StdIn.readLine()
-      initialTerms = initial.split(" ").filter(!_.isBlank).map(Term(_))
-      if initialTerms.isEmpty then println("Empty list, try again")
+  while targetTerms.isEmpty do
+    print("Space separated list of terms for final state: ")
+    val target = "use_jeep" // scala.io.StdIn.readLine()
+    targetTerms = target.split(" ").filter(!_.isBlank).map(Term(_))
+    if targetTerms.isEmpty then println("Empty list, try again")
 
-    while targetTerms.isEmpty do
-      print("Space separated list of terms for final state: ")
-      val target = scala.io.StdIn.readLine()
-      targetTerms = target.split(" ").filter(!_.isBlank).map(Term(_))
-      if targetTerms.isEmpty then println("Empty list, try again")
+  val searchData = SearchData(State(initialTerms), State(targetTerms))
 
-    val searchData =
-      SearchData(State(initialTerms.toSet), State(targetTerms.toSet))
+  println(searchData)
 
-    println(searchData)
-
-    search.search(searchData) match
-      case Some(graph) =>
-        println("\nTarget state is reachable\n")
-      Serializer(graph).serialize("graph.dot", searchData) match
-          case Success(_) => println("\nDecision graph saved\n")
-          case Failure(e) =>
-            println(s"\nUnable to save graph: ${e.getMessage}\n")
-      case None => println("\nTarget state is unreachable\n")
+  BreadthSearch.search(searchData, system) match
+    case Some(rules) =>
+      println("\nTarget state is reachable\n")
+      ToDotFileSerializer.serialize("graph.dot", rules, searchData) match
+        case Success(_) => println("\nDecision graph saved\n")
+        case Failure(e) => println(s"\nUnable to save graph: ${e.getMessage}\n")
+    case None => println("\nTarget state is unreachable\n")
 
 @main def hello: Unit =
   val path = "./system.xml"
   print(s"\nProduction system xml representation file path [$path]: ")
-  val customPath = scala.io.StdIn.readLine()
+  val customPath = "" // scala.io.StdIn.readLine()
 
   Parser(if customPath.isBlank then path else customPath).parse match
     case Success(system) => loop(system)
